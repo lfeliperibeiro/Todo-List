03e51476c40ffa4694820e3670506b3c55cea3aa
Felipe Ribeiro <piporibeiro@gmail.com>
2022-06-18 22:27:45 -0300

    refactor: change name Task for TaskCount

---
+++ src/components/TaskCount/index.tsx
@@ -0,0 +1,27 @@
+import styles from './tasks.module.css'
+interface TaskCountProps {
+  task: number
+  completeTask: number
+}
+export const TaskCount = ({ task, completeTask }: TaskCountProps) => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.createTasks}>
+          <p>Tarefas criadas</p>
+          <span>{task}</span>
+        </div>
+        <div className={styles.doneTasks}>
+          <p>Concluídas</p>
+          {task > 0 ? (
+            <span
+              className={styles.doneTaskCount}
+            >{`${task} de ${completeTask}`}</span>
+          ) : (
+            <span className={styles.createTasksCount}>{task}</span>
+          )}
+        </div>
+      </header>
+    </div>
+  )
+}
