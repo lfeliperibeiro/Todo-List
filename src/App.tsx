b2b1dd7ffe4440e1c4abb05f651ca6b62ebe3e6b
Felipe Ribeiro <piporibeiro@gmail.com>
2022-06-21 20:58:07 -0300

    feat: create functions for todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,65 @@
 import './global.css'
 import { Header } from './components/Header'
-import { Input } from './components/Input'
+import { TodoForm } from './components/TodoForm'
 import { TaskCount } from './components/TaskCount'
 import { useState } from 'react'
 import { NoContent } from './components/NoContent'
+import { Todo } from './components/Todo'
+
+export interface Tasks {
+  id: string
+  text: string
+  isComplete?: boolean
+}
 
 function App() {
-  const [task, setTask] = useState(0)
+  const [todos, setTodos] = useState<Tasks[]>([])
+
+  const addTodo = (todo: Tasks) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return
+    }
+
+    const newTodos = [todo, ...todos]
+
+    setTodos(newTodos)
+  }
+
+  const removeTodo = (id: string) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id)
+
+    setTodos(removeArr)
+  }
+
+  const completeTodo = (id: string) => {
+    let updateTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete
+      }
+      return todo
+    })
+
+    setTodos(updateTodos)
+  }
+
   return (
     <div>
       <Header />
       <div className={'App'}>
-        <Input />
-        <TaskCount task={task} completeTask={0} />
-        <NoContent />
+        <TodoForm onSubmit={addTodo} />
+        <TaskCount
+          task={todos.length}
+          completeTask={todos.filter((item) => item.isComplete === true).length}
+        />
+        {todos.length > 0 ? (
+          <Todo
+            todos={todos}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ) : (
+          <NoContent />
+        )}
       </div>
     </div>
   )
